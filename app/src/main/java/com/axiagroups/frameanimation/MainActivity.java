25829983c993a98bcff5abb3a6d75b2514dcab8b
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-05-07 17:28:59 +0600

    frame animation done

--- app/src/main/java/com/axiagroups/frameanimation/MainActivity.java
+++ app/src/main/java/com/axiagroups/frameanimation/MainActivity.java
@@ -1,6 +1,10 @@
 package com.axiagroups.frameanimation;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private AnimationDrawable batAnimation;
+    private ImageView batImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        batImage = findViewById(R.id.imageBat);
+        batImage.setBackgroundResource(R.drawable.bat_anim);
+        batAnimation = (AnimationDrawable) batImage.getBackground();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        batAnimation.start();
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                batAnimation.stop();
+            }
+        }, 5000);
+        return super.onTouchEvent(event);
+
     }
 }
